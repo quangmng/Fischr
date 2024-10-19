423a66b95e4bac9e6d20f06c69b9bf7a0841577e
Quang Minh Nguyen <quangng711@outlook.com.au>
2024-10-19 15:12:19 +1100

    Initial Commit

---
+++ Fischr/FischrApp.swift
@@ -0,0 +1,20 @@
+//
+//  FischrApp.swift
+//  Fischr
+//
+//  Created by Quang Minh Nguyen on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FischrApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
