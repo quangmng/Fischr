eb8c679c9572d09258a691e569019b10ffe44768
Wendy <lovelywendezhou@163.com>
2024-10-28 14:05:26 +1100

    new HomeView

--- Fischr/View/HomeView.swift
+++ Fischr/View/HomeView.swift
@@ -8,57 +8,73 @@
 import SwiftUI
 
 struct HomeView: View {
+	
+	@State var Gen = GenerateViewModel()
+	@State var getStarted: Bool = false
+	@Binding var isFavourite: Bool
+	
 	var body: some View {
 		NavigationStack {
-			VStack() {
-				HStack {
-					Text("Sharpen \nyour skills \nand \nchallenge \nyour mind!")
-						.font(.custom("VoidBold", size: 40))
+			VStack {
+				NavigationLink {
+					GenerateView(isFavourite: $isFavourite)
+				} label: {
+					getStartedView()
+				}
+				
+				VStack {
+					Text("At A Glance")
 						.font(.title)
 						.bold()
-					Spacer().frame(width: 60*3)
-				}
-				
-				Spacer().frame(height: 230)
-				
-				ZStack {
-					RoundedRectangle(cornerRadius: 20)
-						.fill(Color.red)
-						.frame(width: 150, height: 150)
-						.offset(x: 80)  // Shift to the left
+						.leading()
 					
-					RoundedRectangle(cornerRadius: 20)
-						.fill(Color.blue)
-						.frame(width: 150, height: 150)
-					
-					RoundedRectangle(cornerRadius: 20)
-						.fill(Color.yellow)
-						.frame(width: 150, height: 150)
-						.offset(x: -80)   // Shift to the right
-				}
-				.padding()
-			}
-			.toolbar {
-				ToolbarItem(placement: .topBarLeading) {
-					HStack {
-						Spacer(minLength: 12)
-						Image(systemName: "person")
-							.font(.title2)
+					Section("SAVED POSITIONS") {
+						LazyHStack (spacing: 20) {
+							ForEach(Gen.last10Gen, id: \.self) { gen in
+								HistoryItemView(isFavourite: $isFavourite)
+							}
+						}
 					}
+					.leading()
 				}
-				
-				ToolbarItem(placement: .topBarTrailing) {
-					HStack {
-						Image(systemName: "gear")
-							.font(.title2)
-						Spacer(minLength: 12)
-					}
-				}
+				.padding(20)
 			}
+			
+			
+			.navigationTitle("Fischr")
 		}
 	}
 }
 
 #Preview {
-	HomeView()
+	HomeView(isFavourite: .constant(true))
+}
+
+struct getStartedView: View {
+	var body: some View {
+		ZStack(alignment: .bottomLeading) {
+			ZStack {
+				RoundedRectangle(cornerRadius: 20)
+					.foregroundStyle(Color.gray)
+					.frame(width: .infinity, height: 270)
+				
+				Image("AllChess")
+					.resizable()
+					.frame(height: 270)
+				
+			}
+//			.padding()
+			
+			VStack(alignment: .leading) {
+				Text("Get Started")
+					.foregroundStyle(Color.primary)
+					.font(.title)
+					.bold()
+				Text("Sharpen your skills and challenge your mind")
+					.foregroundStyle(Color.primary)
+			}
+			.padding()
+		}
+		.padding(.horizontal,12)
+	}
 }
